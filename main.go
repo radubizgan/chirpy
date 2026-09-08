f5783d594a1247cdf52db2e9726a84d2a557aa13
radubizgan <rbizgan7@gmail.com>
2026-09-08 14:07:04 +0300

    added a new path to png

--- main.go
+++ main.go
@@ -8,7 +8,10 @@ func main() {
 
 mux :=  http.NewServeMux() 
 fs := http.FileServer(http.Dir("."))// servește fișiere din directorul curent
+pictures := http.FileServer(http.Dir("assets"))// servește fișiere din directorul indicat
 mux.Handle("/",fs )  // toate requesturile care încep cu "/" sunt gestionate de fs
+mux.Handle("/assets",pictures )  // toate requesturile care încep cu "/" sunt gestionate de fs
+
 
 s := &http.Server{
 	Addr:           ":8080",
