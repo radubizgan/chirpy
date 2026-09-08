693ab1b5381d04892ffa7c47b8abf5c480dcf60e
radubizgan <rbizgan7@gmail.com>
2026-09-08 13:35:48 +0300

    added fileserver

--- main.go
+++ main.go
@@ -5,12 +5,14 @@ import "net/http"
 func main() {
 
 
+
 mux :=  http.NewServeMux() 
-HandlerMux := mux
+fs := http.FileServer(http.Dir("."))// servește fișiere din directorul curent
+mux.Handle("/",fs )  // toate requesturile care încep cu "/" sunt gestionate de fs
 
 s := &http.Server{
 	Addr:           ":8080",
-	Handler:        HandlerMux,
+	Handler:        mux,
 }
 
 s.ListenAndServe()
